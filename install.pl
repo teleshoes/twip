44f37964b12556825b6271747789a8c9df22fceb
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-10 20:15:37 -0400

    added installing deps

--- install.pl
+++ install.pl
@@ -4,6 +4,9 @@ use warnings;
 
 my $prefix = '/usr/local';
 
+print "handling prereq, Net::Twitter::Lite perl module\n";
+system "sudo apt-get install libnet-twitter-lite-perl";
+
 my $cmd = "sudo cp src/* $prefix/bin"; 
 system "echo $cmd";
 system $cmd;
