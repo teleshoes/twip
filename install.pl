d9b0625f75ae67ef980f34a1e11f0e5731125c31
Elliot Wolk <elliot.wolk@gmail.com>
2012-08-01 22:32:13 -0400

    trying autoflushing to see if that helps get the prompt to show up...

--- install.pl
+++ install.pl
@@ -20,6 +20,7 @@ run "sudo cp src/* $prefix/bin";
 run "sudo cp logrotate.conf /etc/logrotate.d/twip";
 
 run "sudo rm -f $cronDir/twip";
+$|=1;
 print "\nadd twips cronjob to $cronDir? [y/N]\n";
 if(readline STDIN eq "y\n"){
   run "sudo cp cron /etc/cron.daily/twip";
