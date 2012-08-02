5cc9f67d2f51ce0ebca29694c855889574b531bb
Elliot Wolk <elliot.wolk@gmail.com>
2012-08-01 22:26:23 -0400

    added optional cron job that runs twips to /etc/cron.daily

--- install.pl
+++ install.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 my $prefix = '/usr/local';
+my $cronDir = "/etc/cron.daily";
 
 my @deps = qw(
   libnet-twitter-lite-perl
@@ -16,3 +17,9 @@ sub run(@){
 
 run "sudo apt-get install @deps";
 run "sudo cp src/* $prefix/bin";
+
+run "sudo rm -f $cronDir/twip";
+print "add twips cronjob to $cronDir? [y/N] ";
+if(readline STDIN eq "y\n"){
+  run "sudo cp cron /etc/cron.daily/twip";
+}
